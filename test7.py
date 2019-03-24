738eb69ee5aad81a4757bf438d214224c4a8c7e5
404akhan <404akhan@gmail.com>
2019-03-24 10:58:58 +0600

    changes

--- test7.py
+++ test7.py
@@ -1,9 +1,62 @@
 # create datasets
 
 import csv
+import spacy
+import json
+import numpy as np
 
-with open('my_data_v2/training.1600000.processed.noemoticon.csv') as csv_file:
-	csv_read = csv.reader(csv_file, delimiter=',')
+
+nlp = spacy.load('en')
 
 
-print(csv_read[0])
+fname = 'my_data_v2/training.1600000.processed.noemoticon.csv'
+f = open(fname, 'r', errors='ignore')
+reader = csv.reader(f)
+
+arr = []
+for row in reader:
+	arr.append(row)
+
+
+def filter(token):
+	if token[0] == '@':
+		return '<at_@>'
+	if token[:4] == 'http':
+		return '<http>'
+	return token.lower()
+
+
+def label_change(label):
+	if label == "4":
+		return 'positive'
+	if label == "2":
+		return 'neutral'
+	if label == "0":
+		return 'negative'
+	return 'ERROR_NO_LABEL'
+
+
+def main(fname_out='my_data_v2/data.json'):
+	counter_dict = {'positive': 0, 'neutral': 0, 'negative': 0, 'ERROR_NO_LABEL': 0}
+	open(fname_out, 'w').close()
+
+	with open(fname_out, 'w') as outfile:  
+		row_count = len(arr)
+		indexes = np.random.permutation(row_count)
+		for counter, i in enumerate(indexes):
+			row = arr[i]
+
+			tokenized = [filter(tok.text) for tok in nlp.tokenizer(row[-1])]
+			label = label_change(row[0])
+			counter_dict[label] += 1
+
+			data = {'post': tokenized, 'label': label}
+			json.dump(data, outfile)
+			outfile.write("\n")
+	print(counter_dict)
+
+
+# main()
+
+f.close()
+
---
+++ test8.py
@@ -0,0 +1,251 @@
+# do fastext on our new big data
+
+
+def generate_bigrams(x):
+    n_grams = set(zip(*[x[i:] for i in range(2)]))
+    for n_gram in n_grams:
+        x.append(' '.join(n_gram))
+    return x
+
+print(generate_bigrams(['This', 'film', 'is', 'terrible']))
+
+
+import torch
+from torchtext import data
+from torchtext import datasets
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+
+
+
+
+POST = data.Field(preprocessing=generate_bigrams)
+LABEL = data.LabelField()
+
+
+fields = {'post': ('text', POST), 'label': ('label', LABEL)}
+
+
+train_data, _ = data.TabularDataset.splits(
+                            path = 'my_data_v2',
+                            train = 'small_data.json',
+                            test = 'fake.json',
+                            format = 'json',
+                            fields = fields
+)
+
+
+import random
+
+train_data, test_data = train_data.split(random_state=random.seed(SEED))
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+
+
+
+POST.build_vocab(train_data, max_size=25000, vectors="glove.6B.100d", unk_init=torch.Tensor.normal_)
+LABEL.build_vocab(train_data)
+
+
+
+
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), sort_key=lambda x: len(x.text),
+    batch_size=BATCH_SIZE,
+    device=device)
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FastText(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, output_dim):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.fc = nn.Linear(embedding_dim, output_dim)
+        
+    def forward(self, text):
+        
+        #text = [sent len, batch size]
+        
+        embedded = self.embedding(text)
+                
+        #embedded = [sent len, batch size, emb dim]
+        
+        embedded = embedded.permute(1, 0, 2)
+        
+        #embedded = [batch size, sent len, emb dim]
+        
+        pooled = F.avg_pool2d(embedded, (embedded.shape[1], 1)).squeeze(1) 
+        
+        #pooled = [batch size, embedding_dim]
+                
+        return self.fc(pooled)
+
+
+INPUT_DIM = len(POST.vocab)
+EMBEDDING_DIM = 100
+OUTPUT_DIM = 2
+
+model = FastText(INPUT_DIM, EMBEDDING_DIM, OUTPUT_DIM)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print('The model has %d trainable parameters' % count_parameters(model))
+
+
+
+pretrained_embeddings = POST.vocab.vectors
+
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+
+import torch.optim as optim
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.CrossEntropyLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+def categorical_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    max_preds = preds.argmax(dim=1, keepdim=True) # get the index of the max probability
+    correct = max_preds.squeeze(1).eq(y)
+    return correct.sum()/torch.FloatTensor([y.shape[0]])
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        
+        optimizer.zero_grad()
+        
+        predictions = model(batch.text).squeeze(1)
+        
+        loss = criterion(predictions, batch.label)
+        
+        acc = categorical_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+
+            predictions = model(batch.text)
+            
+            loss = criterion(predictions, batch.label)
+            
+            acc = categorical_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+import time
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+
+N_EPOCHS = 10
+
+best_valid_loss = float('inf')
+
+print('start')
+print(vars(train_data.examples[0]))
+
+print("Unique tokens in POST vocabulary: %d" % len(POST.vocab))
+print("Unique tokens in LABEL vocabulary: %d" % len(LABEL.vocab))
+
+print(POST.vocab.freqs.most_common(20))
+print(POST.vocab.itos[:10])
+print(LABEL.vocab.stoi)
+
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+    
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+    
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'test8-model.pt')
+    
+    print('Epoch: %d | Epoch Time: %dm %ds' % (epoch+1, epoch_mins, epoch_secs))
+    print('\tTrain Loss: %.3f | Train Acc: %.2f%%' % (train_loss, train_acc*100))
+    print('\t Val. Loss: %.3f |  Val. Acc: %.2f%%' % (valid_loss, valid_acc*100))
+
+
+model.load_state_dict(torch.load('test8-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print('Test Loss: %.3f | Test Acc: %.2f%%' % (test_loss, test_acc*100))
+
+
+
+import spacy
+nlp = spacy.load('en')
+
+def predict_sentiment(sentence, min_len=4):
+    tokenized = generate_bigrams([tok.text.lower() for tok in nlp.tokenizer(sentence)])
+    if len(tokenized) < min_len:
+        tokenized += ['<pad>'] * (min_len - len(tokenized))
+    indexed = [POST.vocab.stoi[t] for t in tokenized]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(1)
+    preds = model(tensor)
+    max_preds = preds.argmax(dim=1)
+    sentiment = LABEL.vocab.itos[max_preds.item()]
+    return sentence, sentiment
+
+
+print(predict_sentiment("This film is terrible"))
+
+print(predict_sentiment("This film is great"))
