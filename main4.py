1f833b36a8a28d8b5598dbbc008f144d5fdffbe2
404akhan <404akhan@gmail.com>
2019-03-24 21:44:52 +0600

    cnn model added

--- main4.py
+++ main4.py
@@ -35,53 +35,6 @@ train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout):
-        super().__init__()
-        
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.conv_0 = nn.Conv2d(in_channels = 1, out_channels = n_filters, 
-                                kernel_size = (filter_sizes[0], embedding_dim))
-        self.conv_1 = nn.Conv2d(in_channels = 1, out_channels = n_filters, 
-                                kernel_size = (filter_sizes[1], embedding_dim))
-        self.conv_2 = nn.Conv2d(in_channels = 1, out_channels = n_filters, 
-                                kernel_size = (filter_sizes[2], embedding_dim))
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, text):
-        
-        #text = [sent len, batch size]
-        
-        text = text.permute(1, 0)
-                
-        #text = [batch size, sent len]
-        
-        embedded = self.embedding(text)
-                
-        #embedded = [batch size, sent len, emb dim]
-        
-        embedded = embedded.unsqueeze(1)
-        
-        #embedded = [batch size, 1, sent len, emb dim]
-        
-        conved_0 = F.relu(self.conv_0(embedded).squeeze(3))
-        conved_1 = F.relu(self.conv_1(embedded).squeeze(3))
-        conved_2 = F.relu(self.conv_2(embedded).squeeze(3))
-            
-        #conv_n = [batch size, n_filters, sent len - filter_sizes[n]]
-        
-        pooled_0 = F.max_pool1d(conved_0, conved_0.shape[2]).squeeze(2)
-        pooled_1 = F.max_pool1d(conved_1, conved_1.shape[2]).squeeze(2)
-        pooled_2 = F.max_pool1d(conved_2, conved_2.shape[2]).squeeze(2)
-        
-        #pooled_n = [batch size, n_filters]
-        
-        cat = self.dropout(torch.cat((pooled_0, pooled_1, pooled_2), dim=1))
-
-        #cat = [batch size, n_filters * len(filter_sizes)]
-            
-        return self.fc(cat)
 
 
 class CNN(nn.Module):
@@ -123,50 +76,6 @@ class CNN(nn.Module):
         
         cat = self.dropout(torch.cat(pooled, dim=1))
 
-        #cat = [batch size, n_filters * len(filter_sizes)]
-            
-        return self.fc(cat)
-
-
-class CNN1d(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout):
-        super().__init__()
-        
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.convs = nn.ModuleList([
-                                    nn.Conv1d(in_channels = embedding_dim, out_channels = n_filters, 
-                                              kernel_size = fs)
-                                    for fs in filter_sizes
-                                    ])
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, text):
-        
-        #text = [sent len, batch size]
-        
-        text = text.permute(1, 0)
-        
-        #text = [batch size, sent len]
-        
-        embedded = self.embedding(text)
-                
-        #embedded = [batch size, sent len, emb dim]
-        
-        embedded = embedded.permute(0, 2, 1)
-        
-        #embedded = [batch size, emb dim, sent len]
-        
-        conved = [F.relu(conv(embedded)) for conv in self.convs]
-            
-        #conv_n = [batch size, n_filters, sent len - filter_sizes[n] - 1]
-        
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        
-        #pooled_n = [batch size, n_filters]
-        
-        cat = self.dropout(torch.cat(pooled, dim=1))
-        
         #cat = [batch size, n_filters * len(filter_sizes)]
             
         return self.fc(cat)
